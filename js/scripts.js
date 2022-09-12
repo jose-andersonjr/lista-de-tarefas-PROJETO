a81c5b8291c14b3f960595999b2ecd6a00c52753
José Anderson <jose.andersonjr2@gmail.com>
2022-09-12 12:38:05 -0300

    Adicionando estilo e as primeiras funções js

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// função que adiciona tarefa
+function addTask(){
+    // título da tarefa
+    const taskTitle = document.querySelector('#task-title').value //pegamos o valor que será enviado nesse input
+
+    if(taskTitle) {
+        //clona template
+        const template = document.querySelector('.template')
+
+        const newTask = template.cloneNode(true)
+
+        //adiciona titulo 
+        newTask.querySelector('.task-title').textContent = taskTitle
+        
+        //remover as classes desnecessárias
+        newTask.classList.remove('template')
+        newTask.classList.remove('hide')
+
+        //adiciona tarefa na lista
+        const list = document.querySelector('#task-list')
+        list.appendChild(newTask)
+
+    }
+
+}
+
+// evento de adicionar tarefa
+const addBtn = document.querySelector('#add-btn')
+
+addBtn.addEventListener('click', function(e) {
+
+    e.preventDefault()
+    addTask()
+
+})
